7b534a610a7cdebd2541ef1401e57c51566a80d7
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-24 20:50:02 +0400

    fix: formatting

--- src/components/layouts/BaseLayout.vue
+++ src/components/layouts/BaseLayout.vue
@@ -6,35 +6,34 @@
     Check assets/layout.css for details
 -->
 <template>
-    <div class="app">
-      <header class="header"></header>
-      <main class="main">
-        <slot></slot>
-      </main>
-      <footer class="footer"></footer>
-    </div>
-  </template>
-  
-  <style scoped>
-  .app {
-    min-height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .header {
-    min-height: 100px;
-    background-color: black;
-  }
-  
-  .main {
-    flex: 1 0 auto;
-    min-height: 1000px;
-  }
-  
-  .footer {
-    min-height: 100px;
-    background-color: black;
-  }
-  </style>
-  
+  <div class="app">
+    <header class="header"></header>
+    <main class="main">
+      <slot></slot>
+    </main>
+    <footer class="footer"></footer>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  min-height: 100px;
+  background-color: black;
+}
+
+.main {
+  flex: 1 0 auto;
+  min-height: 1000px;
+}
+
+.footer {
+  min-height: 100px;
+  background-color: black;
+}
+</style>
