5207a2b0217d10f0610f17266d6420f83db4d73d
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-24 17:26:55 +0400

    feat: add base for modal-friendly layout

---
+++ src/components/layouts/BaseLayout.vue
@@ -0,0 +1,40 @@
+<!-- 
+    Layout that mimics position: fixed for pop-overs, 
+    but allows to use position: absolute instead 
+    for user-friendly zooming on mobile devices.
+
+    Check assets/layout.css for details
+-->
+<template>
+    <div class="app">
+      <header class="header"></header>
+      <main class="main">
+        <slot></slot>
+      </main>
+      <footer class="footer"></footer>
+    </div>
+  </template>
+  
+  <style scoped>
+  .app {
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .header {
+    min-height: 100px;
+    background-color: black;
+  }
+  
+  .main {
+    flex: 1 0 auto;
+    min-height: 1000px;
+  }
+  
+  .footer {
+    min-height: 100px;
+    background-color: black;
+  }
+  </style>
+  
