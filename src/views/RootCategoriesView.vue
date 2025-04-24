fc7c621c621d95986be73ed9650fb9ad449858af
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-24 20:47:06 +0400

    feat: add root categories view

---
+++ src/views/RootCategoriesView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { useCategories } from '../composables/api/useCategories'
+import PaginationControl from '../components/ui-kit/PaginationControl.vue'
+import { usePaginationWithRouter } from '../composables/ui/usePaginationWithRouter'
+
+const {
+  limitsWhitelist,
+  currentLimit,
+  currentPage,
+  pagesTotal,
+  paginationApiParams,
+
+  setTotal,
+  setLimit,
+} = usePaginationWithRouter(new Set([1, 10, 20, 30]))
+
+const { collection, isLoading, execute } = useCategories(paginationApiParams, { setTotal })
+
+execute()
+</script>
+
+<template>
+  <h1>Root categories</h1>
+  <ul v-if="!isLoading && collection.length">
+    <li v-for="item in collection" :key="item.id">
+      <RouterLink :to="`/categories/${item.id}`">
+        {{ item.seoTitle || item.name }}
+      </RouterLink>
+    </li>
+  </ul>
+  <strong v-else-if="isLoading">LOADING</strong>
+  <p v-else>No data</p>
+
+  <PaginationControl
+    :limit-options="limitsWhitelist"
+    :default-limit="currentLimit"
+    :pages="pagesTotal"
+    :current-page="currentPage"
+    @set-limit="setLimit"
+  />
+</template>
