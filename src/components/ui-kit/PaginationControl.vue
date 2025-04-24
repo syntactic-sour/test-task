a4930a248a872d069d6075c4c08135ca592e85c6
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-24 20:20:52 +0400

    feat: add unstyled pagination component

---
+++ src/components/ui-kit/PaginationControl.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  limitOptions: Set<number>
+  defaultLimit: number
+  pages: number
+  currentPage: number
+}>()
+
+const currentLimit = ref<number>(props.defaultLimit)
+</script>
+
+<template>
+  <div>
+    <label>
+      <span>Items per page:</span>
+      <select v-model="currentLimit" @change="$emit('set-limit', currentLimit)">
+        <option v-for="lim in limitOptions" :key="lim" :value="lim">{{ lim }}</option>
+      </select>
+    </label>
+
+    <ul v-if="pages > 1">
+      <li v-if="currentPage > 1">
+        <RouterLink :to="{ query: { ...$route.query, page: currentPage - 1 } }"> Prev </RouterLink>
+      </li>
+      <li v-for="page in pages" :key="page">
+        <RouterLink :to="{ query: { ...$route.query, page } }">
+          {{ page }}
+        </RouterLink>
+      </li>
+      <li v-if="currentPage < pages">
+        <RouterLink :to="{ query: { ...$route.query, page: currentPage + 1 } }"> Next </RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
