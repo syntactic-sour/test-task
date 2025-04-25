dbaf836b743aa5be2ae2636f5c7c550e80b70529
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-25 11:49:12 +0400

    feat: change interface of a composable

--- src/composables/api/useProducts.ts
+++ src/composables/api/useProducts.ts
@@ -2,11 +2,20 @@ import { computed, ref, watch, type ComputedRef, type Ref } from 'vue'
 import { useFetch } from './useFetch'
 import { useInitMock } from '../mock/useInitMock'
 
-export function useProducts(
-  paginationApiParams: ComputedRef<PaginationAPIPartial>,
-  { setTotal, productsIds }: { setTotal: (total: number) => void; productsIds: Ref<string[]> },
-) {
+export function useProducts({
+  setTotal,
+  productsIds,
+  paginationApiParams,
+}: {
+  setTotal: (total: number) => void
+  productsIds: Ref<string[]>
+  paginationApiParams?: ComputedRef<PaginationAPIPartial>
+}) {
   const idsChunk = computed(() => {
+    if (!paginationApiParams) {
+      return productsIds.value
+    }
+
     if (productsIds.value.length) {
       const start = paginationApiParams.value.offset * paginationApiParams.value.limit
       return productsIds.value.slice(start, start + paginationApiParams.value.limit)
