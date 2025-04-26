2aea373cfe14f00c66eb5e17d698cf94842dfc36
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-26 23:03:38 +0400

    feat: add effect to BuyButton

--- src/components/ui-kit/BuyButton.vue
+++ src/components/ui-kit/BuyButton.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref, onBeforeUnmount } from 'vue'
+
 import { useCartStore } from '@/stores/cart'
 
 const { productId } = defineProps<{
@@ -6,32 +8,59 @@ const { productId } = defineProps<{
 }>()
 
 const { addProduct } = useCartStore()
+const effect = ref(false)
+const timeout = ref()
 
 function handleClick() {
+  clearTimeout(timeout.value)
   addProduct(productId)
+  effect.value = true
+  timeout.value = setTimeout(() => {
+    effect.value = false
+  }, 300)
   // TODO: aria live-region
 }
+
+onBeforeUnmount(() => {
+  clearTimeout(timeout.value)
+})
 </script>
 
 <template>
-  <button type="button" @click="handleClick" class="cta">Add to cart</button>
-  <div class="visually-hidden"></div>
+  <button type="button" @click="handleClick" class="cta" :disabled="effect">
+    Add to cart
+    <Transition name="fade">
+      <span v-if="effect" class="effect"></span>
+    </Transition>
+  </button>
 </template>
 
 <style scoped>
 .cta {
   background: var(--color-text);
   color: var(--color-background);
-  padding: 0.5rem 0.8rem;
+  padding: 0.4em 1.2em;
   border-radius: var(--radius-controls);
   border: none;
   box-shadow: none;
-  font-size: 1.1rem;
-  margin: 0 0.5rem 1rem;
+  font-size: 1.1em;
+  position: relative;
 }
 
 .cta:hover {
   background: var(--color-cta-hover-bg);
   color: var(--color-cta-hover-text);
+}
+
+.effect {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  border-radius: inherit;
+  box-shadow: 0 0 10px 2px var(--color-price-accent);
+  background: var(--color-price-accent);
+  z-index: -1;
 }
 </style>
