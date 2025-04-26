57b7f5a4d8eb24b698bcfff3a37d0b90355851ce
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-26 23:02:13 +0400

    feat: use WithLoader in CategoriesList

--- src/components/CategoriesList.vue
+++ src/components/CategoriesList.vue
@@ -1,32 +1,22 @@
 <script setup lang="ts">
-import DotsLoader from '@/components/ui-kit/DotsLoader.vue'
+import WithLoader from '@/components/ui-kit/WithLoader.vue'
 
 const { collection, isLoading } = defineProps<{ collection: CategoryItem[]; isLoading: boolean }>()
 </script>
 
 <template>
-  <div class="list-container">
-    <Transition name="fade">
-      <DotsLoader v-if="isLoading" ariaLabel="Loading content" class="loader" />
-    </Transition>
-    <Transition name="fade">
-      <ul v-if="!isLoading" class="categories-list">
-        <li v-for="item in collection" :key="item.id" class="categories-item">
-          <RouterLink :to="`/categories/${item.id}`">
-            {{ item.seoTitle || item.name }}
-          </RouterLink>
-        </li>
-      </ul>
-    </Transition>
-  </div>
+  <WithLoader :isLoading="isLoading" loaderLabel="Loading content">
+    <ul class="categories-list">
+      <li v-for="item in collection" :key="item.id" class="categories-item">
+        <RouterLink :to="`/categories/${item.id}`">
+          {{ item.seoTitle || item.name }}
+        </RouterLink>
+      </li>
+    </ul>
+  </WithLoader>
 </template>
 
 <style scoped>
-.list-container {
-  position: relative;
-  min-height: 2rem;
-}
-
 .categories-list {
   margin: 0;
   padding: 0;
@@ -35,10 +25,5 @@ const { collection, isLoading } = defineProps<{ collection: CategoryItem[]; isLo
 
 .categories-item + .categories-item {
   margin-top: 0.5rem;
-}
-
-.loader {
-  position: absolute;
-  font-size: 2rem;
 }
 </style>
