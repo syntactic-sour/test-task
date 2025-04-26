94729fa4ebf4d12528a430e5816b0716e5b92997
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-26 23:03:57 +0400

    feat: add grid component

---
+++ src/components/ui-kit/ItemsGrid.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts" generic="T">
+interface Item {
+  id: string | number
+}
+
+type Collection = Array<Item & T>
+
+const { collection } = defineProps<{ collection: Collection }>()
+</script>
+
+<template>
+  <ul class="collection">
+    <li class="item" v-for="item in collection" :key="item.id">
+      <slot :item="item"></slot>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.collection {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
+}
+
+.item {
+  display: flex;
+}
+</style>
