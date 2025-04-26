22414000de2d711d184a8e776bea7328d5c7ab4d
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-26 23:04:38 +0400

    feat: update product page

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -3,7 +3,10 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 import { useProduct } from '@/composables/api/useProduct'
-import { useCartStore } from '@/stores/cart'
+import { useClientStoreStore } from '@/stores/mock/store'
+
+import WithLoader from '@/components/ui-kit/WithLoader.vue'
+import BuyButton from '@/components/ui-kit/BuyButton.vue'
 
 const route = useRoute()
 
@@ -11,27 +14,60 @@ const productId = computed(() =>
   typeof route.params.productId === 'string' ? route.params.productId : route.params.productId[0],
 )
 
-const { product, execute } = useProduct(productId)
-const { addProduct } = useCartStore()
+const { currency } = useClientStoreStore()
+const { product, isLoading, execute } = useProduct(productId)
+
 execute()
 </script>
 
 <template>
-  <h1>{{ product?.seoTitle || product?.name }}</h1>
-  <picture v-for="pic in product?.media?.images" :key="pic.id">
-    <source media="(max-width:320px)" :srcset="pic.image160pxUrl" />
-    <source media="(max-width:480px)" :srcset="pic.image400pxUrl" />
-    <source media="(max-width:560px)" :srcset="pic.image800pxUrl" />
-    <source media="(max-width:700px)" :srcset="pic.image1500pxUrl" />
+  <WithLoader :is-loading="isLoading" loaderLabel="Loading product">
+    <div v-if="product">
+      <h1>{{ product.seoTitle || product.name }}</h1>
+      <div class="images">
+        <picture v-for="pic in product.media?.images" :key="pic.id">
+          <source media="(max-width:320px)" :srcset="pic.image160pxUrl" />
+          <source media="(max-width:480px)" :srcset="pic.image400pxUrl" />
+          <source media="(max-width:560px)" :srcset="pic.image800pxUrl" />
+          <source media="(max-width:700px)" :srcset="pic.image1500pxUrl" />
 
-    <img
-      :src="pic.image1500pxUrl"
-      alt="???????????? No description for images? ATAG"
-      style="width: auto"
-    />
-  </picture>
-  <strong>{{ product?.price }}</strong>
-  <button :disabled="!product" type="button" @click="() => product && addProduct(product.id)">
-    Buy
-  </button>
+          <img :src="pic.image1500pxUrl" alt="" style="width: auto" />
+        </picture>
+      </div>
+      <div class="shopping-container">
+        <strong class="price">{{ product?.price }} {{ currency }}</strong>
+
+        <BuyButton :productId="product?.id" />
+      </div>
+    </div>
+  </WithLoader>
+
+  <Transition name="fade">
+    <div v-if="!isLoading && !product">
+      <h1>Product not found</h1>
+    </div>
+  </Transition>
 </template>
+
+<style scoped>
+.images {
+  margin: 2rem 0;
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(9rem, 1fr));
+}
+
+.shopping-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 2.8rem;
+  font-size: 1.4rem;
+}
+
+.price {
+  font-size: 2rem;
+  font-weight: bold;
+  color: var(--color-price-accent);
+}
+</style>
