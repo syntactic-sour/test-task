9d449d8579618e6474548d1813fba37422d5e397
syntactic.sour <ssharipova.fe@gmail.com>
2025-04-27 18:01:25 +0400

    feat: add component for product amount in cart, increase and decrease amount

---
+++ src/components/ProductAmount.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
+import { useCartStore } from '@/stores/cart'
+
+import SecondaryButton from '@/components/ui-kit/SecondaryButton.vue'
+
+const { product } = defineProps<{ product: Product }>()
+
+const cart = useCartStore()
+const { productsInCart } = storeToRefs(cart)
+</script>
+
+<template>
+  <SecondaryButton @click="() => cart.removeProduct(product.id, 1)" arial-label="Decrease amount">
+    −
+  </SecondaryButton>
+  <span class="amount">{{ productsInCart[product.id] }}</span>
+  <SecondaryButton @click="() => cart.addProduct(product.id, 1)" aria-label="Increase amount">
+    +
+  </SecondaryButton>
+</template>
+
+<style scoped>
+.amount {
+  padding: 0 0.5em;
+}
+</style>
